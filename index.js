1384db120083df5765f13ad2ea6619bc4c448fd5
JanainaNogueira <janainanogueira464@gmail.com>
2022-08-20 20:30:51 -0300

    feat:Check the month selected in the next-month and last-month functions
    -Added a conditional check where the vector of months is directed to
        the first or last item depending on from your current position by
        clicking forwards or backwards through the months and undefined
        does not occur.

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
+//Captura a data atual
 const months = ["Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto",
                 "Setembro","Outubro","Novembro","Dezembro"];
 var date = new Date();
 var month = date.getMonth();
 
+//avançar e retroceder meses
 function last_month(){
-    month++
-    console.log(month);
+    if(month<=0){
+        month=11;
+    }else{
+    month--
+    }
+    month_name.innerHTML = months[month];
 }
 function next_month(){
-    month--
-    console.log(month);
+    if(month>=11){
+        month=0;
+    }else{
+      month++  
+    }
+    month_name.innerHTML=months[month];
 }
+
+//Pega as divs de avançar e retroceder os meses
 document.getElementById('last-month').addEventListener("click",last_month);
 document.getElementById('next-month').addEventListener("click",next_month);
+
+//Pega o titulo do mês
+var month_name = document.getElementById("month-name")
