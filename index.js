f64ff5d25c57af38f5d1a1116a6c3e847d091fe2
JanainaNogueira <janainanogueira464@gmail.com>
2022-08-21 20:09:26 -0300

    feat(index.js): Create update_month function
    - The update_month function adds the days in the "td" tags based on
        the selected month and year.
    - Added incrementer and decrementator of the year variable in the
        last_month and next_month functions.

--- index.js
+++ index.js
@@ -1,29 +1,42 @@
 //Captura a data atual
 const months = ["Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto",
                 "Setembro","Outubro","Novembro","Dezembro"];
-var date = new Date();
-var month = date.getMonth();
+let date = new Date();
+let month = date.getMonth();
+let year = date.getFullYear();
 
 function load(){
     month_name.innerHTML=months[month];
+    update_month(month, year);
+}
+function update_month(month,year){
+    let firstDay = new Date(year, month, 1).getDay()-1;
+    for(var i=-firstDay,index=0;i<(42-firstDay);i++,index++){
+        let day = new Date(year,month,i).getDate()
+        document.getElementsByTagName('td')[index].innerHTML= day;
+    }
 }
 
 //avançar e retroceder meses
 function last_month(){
     if(month<=0){
         month=11;
+        year--
     }else{
     month--
     }
     month_name.innerHTML = months[month];
+    update_month(month,year)
 }
 function next_month(){
     if(month>=11){
         month=0;
+        year++
     }else{
       month++  
     }
     month_name.innerHTML=months[month];
+    update_month(month,year)
 }
 
 //Pega as divs de avançar e retroceder os meses
